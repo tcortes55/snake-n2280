051ccf191333e01d994f5687826d02e6d8169cef
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2018-11-13 20:05:51 +0100

    Upload to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { COLORS, CONTROLS, BOARD_WIDTH, BOARD_HEIGHT } from './app.constants';
 
 @Component({
-  selector: 'app-root',
+  selector: 'snake-n2280',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'snake-n2280';
+
+  
+  public board = [];
+
+
+  private snake = {
+    direction: CONTROLS.LEFT,
+    parts: [
+      {
+        // Initially it is not displayed
+        x: -1,
+        y: -1
+      }
+    ]
+  };
+
+
+  private fruit = {
+    // Initially it is not displayed
+    x: -1,
+    y: -1
+  };
+
+
+
+
+
+  constructor() {
+    this.setBoard();
+  }
+
+
+
+  setBoard(): void {
+    this.board = [];
+
+    for (let i = 0; i < BOARD_HEIGHT; i++) {
+      this.board[i] = [];
+      for (let j = 0; j <  BOARD_WIDTH; j++)
+      {
+        this.board[i][j] = false; // indicates that the snake is not currently in this location
+      }
+    }
+
+  }
+
+  setColors(col: number, row: number): string {
+    // if (this.board[col][row] === true) {
+    //   return COLORS.BODY;
+    // }
+
+    if (this.fruit.x === row && this.fruit.y === col) {
+      return COLORS.FRUIT;
+    } else if (this.snake.parts[0].x === row && this.snake.parts[0].y === col) {
+      return COLORS.HEAD;
+    } else if (this.board[col][row] === true) {
+      return COLORS.BODY;
+    }
+
+    return COLORS.BOARD;
+  };
+
 }
---
+++ src/app/app.constants.ts
@@ -0,0 +1,18 @@
+export const BOARD_WIDTH = 16;
+export const BOARD_HEIGHT = 8;
+
+export const CONTROLS = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+  };
+
+export const COLORS = {
+    GAME_OVER: '#D24D57',
+    FRUIT: '#EC644B',
+    HEAD: '#336E7B',
+    BODY: '#C8F7C5',
+    BOARD: '#86B5BD',
+    OBSTACLE: '#383522'
+  };
