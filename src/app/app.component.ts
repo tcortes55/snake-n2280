e36e38b59598bf2f43a909e31dce547020f94c3f
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2018-11-15 01:15:27 +0100

    First functional version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,19 @@ import { COLORS, CONTROLS, BOARD_WIDTH, BOARD_HEIGHT } from './app.constants';
 @Component({
   selector: 'snake-n2280',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(document:keydown)': 'handleKeyboardEvents($event)'
+  }
 })
 export class AppComponent {
   title = 'snake-n2280';
 
+  private interval: number;
+  private tempDirection: number;
   
   public board = [];
+  public score = 0;
 
 
   private snake = {
@@ -24,7 +30,6 @@ export class AppComponent {
     ]
   };
 
-
   private fruit = {
     // Initially it is not displayed
     x: -1,
@@ -32,15 +37,128 @@ export class AppComponent {
   };
 
 
-
-
-
   constructor() {
     this.setBoard();
   }
 
+  handleKeyboardEvents(e: KeyboardEvent) {
+    if (e.keyCode === CONTROLS.LEFT && this.snake.direction != CONTROLS.RIGHT) {
+      this.tempDirection = CONTROLS.LEFT;
+    } else if (e.keyCode === CONTROLS.UP && this.snake.direction != CONTROLS.DOWN) {
+      this.tempDirection = CONTROLS.UP;
+    } else if (e.keyCode === CONTROLS.RIGHT && this.snake.direction != CONTROLS.LEFT) {
+      this.tempDirection = CONTROLS.RIGHT;
+    } else if (e.keyCode === CONTROLS.DOWN && this.snake.direction != CONTROLS.UP) {
+      this.tempDirection = CONTROLS.DOWN;
+    } 
+  }
 
 
+
+
+  setColors(col: number, row: number): string {
+    if (this.fruit.x === row && this.fruit.y === col) {
+      return COLORS.FRUIT;
+    } else if (this.snake.parts[0].x === row && this.snake.parts[0].y === col) {
+      return COLORS.SNAKE_HEAD;
+    } else if (this.board[col][row] === true) {
+      return COLORS.SNAKE_BODY;
+    }
+
+    return COLORS.BOARD;
+  };
+
+  fruitCollision(part: any): boolean {
+    return (part.x === this.fruit.x && part.y === this.fruit.y);
+  }
+
+  eatFruit(): void {
+    let newTail = Object.assign({}, this.snake.parts[this.snake.parts.length - 1]);
+
+    this.snake.parts.push(newTail);
+    this.resetFruit();
+  }
+
+  updatePositions(): void {
+    let newHead = this.repositionHead();
+    let me = this;
+
+    this.headTransition(newHead);
+
+    if (this.fruitCollision(newHead))
+    {
+      this.eatFruit();
+    }
+
+    let lastPart = this.snake.parts.pop();
+    this.board[lastPart.y][lastPart.x] = false;
+
+    this.snake.parts.unshift(newHead);
+    this.board[newHead.y][newHead.x] = true;
+
+    this.snake.direction = this.tempDirection;
+
+    setTimeout(() => {
+      me.updatePositions();
+    }, this.interval);
+  }
+
+  repositionHead(): any {
+    let newHead = Object.assign({}, this.snake.parts[0]);
+
+    if (this.tempDirection === CONTROLS.LEFT) {
+      newHead.x -= 1;
+    } else if (this.tempDirection === CONTROLS.RIGHT) {
+      newHead.x += 1;
+    } else if (this.tempDirection === CONTROLS.UP) {
+      newHead.y -= 1;
+    } else if (this.tempDirection === CONTROLS.DOWN) {
+      newHead.y += 1;
+    }
+
+    return newHead;
+  }
+
+  headTransition(part: any): void {
+    if (part.x === BOARD_WIDTH) {
+      part.x = 0;
+    } else if (part.x === -1) {
+      part.x = BOARD_WIDTH - 1;
+    }
+
+    if (part.y === BOARD_HEIGHT) {
+      part.y = 0;
+    } else if (part.y === -1) {
+      part.y = BOARD_HEIGHT - 1;
+    }
+  }
+
+  resetFruit(): void {
+    let x = this.randomWidth();
+    let y = this.randomHeight();
+
+    if (this.board[y][x] === true)
+    {
+      return this.resetFruit();
+    }
+
+    this.fruit = {
+      x: x,
+      y: y
+    }
+  }
+
+  // Aux
+  randomHeight(): any {
+    return Math.floor(Math.random() * BOARD_HEIGHT);
+  }
+  
+  randomWidth(): any {
+    return Math.floor(Math.random() * BOARD_WIDTH);
+  }
+
+  
+  // Preparing the game
   setBoard(): void {
     this.board = [];
 
@@ -54,20 +172,22 @@ export class AppComponent {
 
   }
 
-  setColors(col: number, row: number): string {
-    // if (this.board[col][row] === true) {
-    //   return COLORS.BODY;
-    // }
-
-    if (this.fruit.x === row && this.fruit.y === col) {
-      return COLORS.FRUIT;
-    } else if (this.snake.parts[0].x === row && this.snake.parts[0].y === col) {
-      return COLORS.HEAD;
-    } else if (this.board[col][row] === true) {
-      return COLORS.BODY;
+  //
+  newGame(): void {
+    this.score = 0;
+    this.interval = 150;
+    this.tempDirection = CONTROLS.LEFT;
+    this.snake = {
+      direction: CONTROLS.LEFT,
+      parts: []
     }
 
-    return COLORS.BOARD;
-  };
+    for (let i = 0; i < 3; i++)
+    {
+      this.snake.parts.push({ x: BOARD_WIDTH/2 + i, y: Math.floor(BOARD_HEIGHT/2)})
+    }
 
+    this.resetFruit();
+    this.updatePositions();
+  }
 }
--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const BOARD_WIDTH = 16;
-export const BOARD_HEIGHT = 8;
+export const BOARD_WIDTH = 18;
+export const BOARD_HEIGHT = 9;
 
 export const CONTROLS = {
     LEFT: 37,
@@ -11,8 +11,8 @@ export const CONTROLS = {
 export const COLORS = {
     GAME_OVER: '#D24D57',
     FRUIT: '#EC644B',
-    HEAD: '#336E7B',
-    BODY: '#C8F7C5',
-    BOARD: '#86B5BD',
+    SNAKE_HEAD: 'rgb(32,43,0)',
+    SNAKE_BODY: 'rgb(75,100,0)',
+    BOARD: 'rgb(155,185,4)',
     OBSTACLE: '#383522'
   };
